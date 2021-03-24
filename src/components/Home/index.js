a830140828c4c6e0ac2909b1e8915e04a8d1cc8b
Matt Kufchak <matt.kufchak@gmail.com>
2021-03-23 23:14:56 -0500

    added font and a photo

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,11 @@
 import './Home.css'
+import image from "./bw.jpg"
 
 export default function Home() {
   return (
-    <h1>Matt Kufchak</h1>
+    <>
+    <h1 className="title_main">Matt Kufchak</h1>
+    <img src={image} className="home__image" alt="Matt Kufchak smiling"></img>
+    </>
   )
 }
